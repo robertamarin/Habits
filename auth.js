451965730f1097c64a83f01ac02452cba829837c
robertamarin <127469693+robertamarin@users.noreply.github.com>
2025-12-31 19:25:03 -0800

    Refactor auth.js to remove unused code
    Removed unused imports and variables related to goals and quits.

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ import {
   signOut
 } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
 import { auth } from './firebase.js';
-import { loadDay, loadHabits, migrateLocal, loadGoals, loadQuits } from './firestore.js';
+import { loadDay, loadHabits, migrateLocal } from './firestore.js';
 
 const provider = new GoogleAuthProvider();
 
@@ -32,57 +32,10 @@ export function observeAuthState(onSignIn, onSignOut) {
     if (user) {
       await migrateLocal(user.uid);
       const habits = await loadHabits(user.uid);
-      const goals = await loadGoals(user.uid);
-      const quits = await loadQuits(user.uid);
       const todayData = await loadDay(user.uid);
-      if (onSignIn) onSignIn(user, { habits, goals, quits, day: todayData });
+      if (onSignIn) onSignIn(user, { habits, day: todayData });
     } else if (onSignOut) {
       onSignOut();
     }
   });
 }
-
-
-
-
-// import {
-//   createUserWithEmailAndPassword,
-//   GoogleAuthProvider,
-//   onAuthStateChanged,
-//   signInWithEmailAndPassword,
-//   signInWithPopup,
-//   signOut
-// } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
-// import { auth } from './firebase.js';
-// import { loadDay, loadHabits, migrateLocal } from './firestore.js';
-
-// const provider = new GoogleAuthProvider();
-
-// export function signUp(email, password) {
-//   return createUserWithEmailAndPassword(auth, email, password);
-// }
-
-// export function signIn(email, password) {
-//   return signInWithEmailAndPassword(auth, email, password);
-// }
-
-// export function signInWithGoogle() {
-//   return signInWithPopup(auth, provider);
-// }
-
-// export function logOut() {
-//   return signOut(auth);
-// }
-
-// export function observeAuthState(onSignIn, onSignOut) {
-//   return onAuthStateChanged(auth, async (user) => {
-//     if (user) {
-//       await migrateLocal(user.uid);
-//       const habits = await loadHabits(user.uid);
-//       const todayData = await loadDay(user.uid);
-//       if (onSignIn) onSignIn(user, { habits, day: todayData });
-//     } else if (onSignOut) {
-//       onSignOut();
-//     }
-//   });
-// }
